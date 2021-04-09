834cb436f97508be96045ea194d2a29338127111
MountVase <markus.vasemagi@gmail.com>
2021-04-09 14:34:35 +0300

    everything else..

--- src/index.js
+++ src/index.js
@@ -1,7 +1,37 @@
-import React from 'react'
-import ReactDom from 'react-dom'
-import 'normalize.css'
-import './index.css'
-import App from './App'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
 
-ReactDom.render(<App />, document.getElementById('root'))
+import { createGlobalStyle, ThemeProvider } from 'styled-components'
+import { GlobalTypeStyles } from 'styled-typography'
+import evening from './assets/evening.jpg'
+
+const GlobalStyle = createGlobalStyle`
+  html,body {
+    margin: 0;
+    padding: 0;
+    height: 400vh;
+
+    background-image: url(${evening});
+    background-size: cover;
+    background-repeat: no-repeat;
+    overflow-x: hidden;
+  }
+`
+
+const typographyTheme = {
+  fontSizes: ["3rem", "1.777rem", "1.333rem", "2rem", "0.75rem", "10px"],
+  bodyFontFamily: "Geometric Sans-Serif",
+  headingFontFamily: "Helvetica Neue, Circular, sans-serif",
+}
+
+ReactDOM.render(
+  <React.StrictMode>
+    <ThemeProvider theme={{ typography: typographyTheme }} >
+      <GlobalStyle />
+      <GlobalTypeStyles />
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
+)
