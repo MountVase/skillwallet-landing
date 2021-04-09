2d0431cb10b9b39831cbb3408f88e2ddb4b5b21a
MountVase <markus.vasemagi@gmail.com>
2021-04-09 14:33:52 +0300

    App.js rendering and styling logic

--- src/App.js
+++ src/App.js
@@ -1,21 +1,50 @@
-import React from 'react'
-import Hero from './Component/Hero/Hero'
-import Header from './Component/Header/Header'
-import LeftSection from './Component/Sections/LeftSection'
-import Footer from './Component/Footer/Footer'
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+import Parallax from "react-rellax";
 
-import Addition from './Component/Hero/Addition'
-import City from './Component/City/'
+import Fade from "react-reveal/Fade";
 
-const App = () => (
-  <>
-    <Header />
-    <Hero />
-    <City />
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Section from "./components/Section"
+import OtherSection from "./components/OtherSection"
 
-    <Addition />
-    <LeftSection />
-    <Footer />
-  </>
-)
-export default App
+const Emptiness = styled.div`
+  display: flex;
+  min-height: 25vh;
+
+`;
+
+const App = () => {
+  const [offsetY, setOffsetY] = useState(0);
+
+  const handleScroll = () => setOffsetY(window.pageYOffset);
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+  }, []);
+
+
+  return (
+    <>
+      <Navbar />
+      <Hero />
+
+
+      <Section />
+
+      <Emptiness />
+
+      <OtherSection />
+
+      <Emptiness />
+
+      <Section />
+
+      <Emptiness />
+
+    </>
+  );
+};
+
+export default App;
